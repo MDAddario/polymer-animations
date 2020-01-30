79004c5fa100a151fe8aed1470df508cc1bc303b
MDAddario <michael.lindner.daddario@gmail.com>
2020-01-30 12:19:12 -0500

    Tidy up main, make main generate positions of polymer after every monomer is updated

--- generate_positions.c
+++ generate_positions.c
@@ -49,33 +49,31 @@ int main(){
 	// Allocate position array
 	double **positions = (double**)malloc(NUM_MONOMERS * sizeof(double*));
 	for (int I = 0; I < NUM_MONOMERS; I++)
-		positions[I] = (double*)malloc(2 * sizeof(double));
+		positions[I] = (double*)malloc(3 * sizeof(double));
 	
-	// Randomly generate polymer I.C.s
-	positions[0][0] = 0;
-	positions[0][1] = 0;
-	
+	// Declare my friendly variables
+	int I;
 	double a = 1.0;
 	double angle;
-	
-	for (int I = 1; I < NUM_MONOMERS; I++){
-		
-		angle = random_angle();
-		positions[I][0] = positions[I-1][0] + a * cos(angle);
-		positions[I][1] = positions[I-1][1] + a * sin(angle);
-	}
-	
-	int I;
 	double disp_x, disp_y;
 	double cosine, sine;
 	double vector_x, vector_y;
 	double rot_vec_x, rot_vec_y;
-	double mag;
 	
-	// End to end distance array
-	double *distances = (double*)malloc(NUM_STEPS * sizeof(double));
+	// Randomly generate polymer I.C.s
+	positions[0][0] = 0.0;
+	positions[0][1] = 0.0;
+	positions[0][2] = 0.0;
 	
-	// Metropolis sampling
+	for (I = 1; I < NUM_MONOMERS; I++){
+		
+		angle = random_angle();
+		positions[I][0] = positions[I-1][0] + a * cos(angle);
+		positions[I][1] = positions[I-1][1] + a * sin(angle);
+		positions[I][2] = 0.0;
+	}
+	
+	// Random pivot sampling
 	for (long step = 0; step < NUM_STEPS; step++){
 		
 		// Pick random angle and index
@@ -84,6 +82,7 @@ int main(){
 		cosine = cos(angle);
 		sine = sin(angle);
 		
+		// Retrieve existing monomer-to-monomer vector
 		vector_x = positions[I+1][0] - positions[I][0];
 		vector_y = positions[I+1][1] - positions[I][1];
 		
@@ -95,36 +94,13 @@ int main(){
 		disp_x = rot_vec_x - vector_x;
 		disp_y = rot_vec_y - vector_y;
 		
+		// Propagate displacement
 		for (int index = I+1; index < NUM_MONOMERS; index++){
 			positions[index][0] += disp_x;
 			positions[index][1] += disp_y;
+			
+			// Save positions
+			saveLineToFile(FILENAME, positions);
 		}
-		
-		// Compute end to end distance
-		vector_x = positions[NUM_MONOMERS-1][0] - positions[0][0];
-		vector_y = positions[NUM_MONOMERS-1][1] - positions[0][1];
-		mag = pow(pow(vector_x, 2) + pow(vector_y, 2), 0.5);
-		distances[step] = mag;
 	}
-	
-	// Save distances to file
-	saveToFile(FILENAME_RAW, NUM_STEPS, 1, distances);
-	
-	// Compute the distances every N steps
-	long size = NUM_STEPS / NUM_MONOMERS;
-	double *distance_every_N = (double*)malloc(size * sizeof(double));
-	for (long i = 0; i < size; i++)
-		distance_every_N[i] = distances[i * NUM_MONOMERS];
-	
-	// Cumulate sum and average
-	for (long i = size - 1; i > 0; i--){
-		for (long j = 0; j < i; j++){
-			distance_every_N[i] += distance_every_N[j] * distance_every_N[j];
-		}
-		distance_every_N[i] =  sqrt(distance_every_N[i] / (i + 1));
-	}
-	
-	// Save distances to file
-	saveToFile(FILENAME_AVG, size, NUM_MONOMERS, distance_every_N);
-	return 0;
 }
