4550b8d1d95cbee0588d67eb5012a0c81d1c2d09
MDAddario <michael.lindner.daddario@gmail.com>
2020-01-30 12:52:59 -0500

    Configure the input data as to not hardcode the number of monomers

--- animate.py
+++ animate.py
@@ -10,17 +10,15 @@ positions_raw = np.loadtxt('monomer_positions.txt', delimiter=',')
 # Trim the trailing zero artifact
 positions_array = positions_raw[:,0:-1]
 
-'''
-FIND WAY TO IMPORT THE NUMBER OF MONOMERS
-'''
-num_monomers = 10
+# Determine how many different conformations there are
+# Determine how many monomers there are
+num_steps = positions_array.shape[0]
+num_monomers = positions_array.shape[1] // 3
 
 # Recast the positions array as a list of positions at every step
 # Also, reshape the positions into a N x 3 array
 shape = (num_monomers, 3)
 positions_list = [np.reshape(row, shape) for row in positions_array]
-
-
 
 exit()
 
