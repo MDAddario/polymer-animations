03bbebec3727af399ba0f2122e297b07978a1b2f
MDAddario <michael.lindner.daddario@gmail.com>
2020-01-31 10:07:58 -0500

    Renders rough histogram, although data is plotted newly every iteration, causing great lag

--- animate.py
+++ animate.py
@@ -9,10 +9,12 @@ LOAD DATA SET
 '''
 
 # Loading the data set
-# Trim the trailing zero artifact
 filename = 'data/monomer_positions.txt'
 positions_raw = np.loadtxt(filename, delimiter=',')
+
+# Isolate positions and end-to-end distances
 positions_array = positions_raw[:,0:-1]
+distances_array = positions_raw[:,-1]
 
 # Determine how many different conformations there are
 # Determine how many monomers there are
@@ -28,21 +30,30 @@ positions_list = [np.reshape(row, shape) for row in positions_array]
 PLOT SYSTEM
 '''
 
-# Initiate axes
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# Initiate 3D axes
+fig = plt.figure(figsize=(10,5))
+ax1 = fig.add_subplot(121, projection='3d')
+ax2 = fig.add_subplot(122)
 
 # Initial positions
-line = ax.plot(positions_list[0][:,0], \
+line = ax1.plot(positions_list[0][:,0], \
 				positions_list[0][:,0], \
 				positions_list[0][:,0],
 				'-o')[0]
+				
+ax2.hist(distances_array[0])
+
+'''
+https://matplotlib.org/gallery/animation/animated_histogram.html
+'''
 
 # Update atom positions
 def update(frame):
 
 	line.set_data(positions_list[frame][:,0:2].T)
 	line.set_3d_properties(positions_list[frame][:,2])
+	
+	ax2.hist(distances_array[0:frame+1])
 
 '''
 CONFIGURE PLOT SETTINGS
@@ -50,10 +61,11 @@ CONFIGURE PLOT SETTINGS
 
 # Set axis limits
 limit = np.sqrt(num_monomers)
-ax.set_xlim3d([-limit, limit])
-ax.set_ylim3d([-limit, limit])
-ax.set_zlim3d([-limit, limit])
-ax.set_title('Freely Jointed 3D Random Walk', fontsize=20)
+ax1.set_xlim3d([-limit, limit])
+ax1.set_ylim3d([-limit, limit])
+ax1.set_zlim3d([-limit, limit])
+ax1.set_title('Freely Jointed 3D Random Walk', fontsize=20)
+ax2.set_xlim([0, num_monomers])
 
 '''
 ANIMATE THE POLYMER
